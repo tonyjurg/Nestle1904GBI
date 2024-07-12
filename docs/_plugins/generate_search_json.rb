016ae007333f92ceff861b6ae798369a073ddb3d
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2024-07-12 13:17:43 +0200

    Create generate_search_json.rb

---
+++ docs/_plugins/generate_search_json.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+module Jekyll
+  class SearchJsonGenerator < Generator
+    safe true
+
+    def generate(site)
+      items = []
+      site.posts.docs.each do |post|
+        items << {
+          title: post.data['title'],
+          url: post.url,
+          content: post.content.gsub(/<\/?[^>]*>/, "")  # Remove HTML tags
+        }
+      end
+
+      File.open('search.json', 'w') do |f|
+        f.write(JSON.pretty_generate(items))
+      end
+    end
+  end
+end
