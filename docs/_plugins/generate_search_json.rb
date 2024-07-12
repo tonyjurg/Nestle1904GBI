61c2ab7b43f7a467bb5932587520ecbde25c8151
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2024-07-12 13:39:22 +0200

    Update generate_search_json.rb

--- docs/_plugins/generate_search_json.rb
+++ docs/_plugins/generate_search_json.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'nokogiri'
 
 module Jekyll
   class SearchJsonGenerator < Generator
@@ -6,15 +7,21 @@ module Jekyll
 
     def generate(site)
       items = []
-      site.posts.docs.each do |post|
+
+      Dir.glob('_site/docs/**/*.html').each do |file|
+        doc = Nokogiri::HTML(File.read(file))
+        content = doc.css('body').text.gsub(/<\/?[^>]*>/, "") # Remove HTML tags
+        title = doc.at_css('title')&.text || 'No Title'
+        url = file.gsub('_site', '')
+
         items << {
-          title: post.data['title'],
-          url: post.url,
-          content: post.content.gsub(/<\/?[^>]*>/, "")  # Remove HTML tags
+          title: title,
+          url: url,
+          content: content
         }
       end
 
-      File.open('../search.json', 'w') do |f|
+      File.open(File.join(site.dest, '../search.json'), 'w') do |f|
         f.write(JSON.pretty_generate(items))
       end
     end
